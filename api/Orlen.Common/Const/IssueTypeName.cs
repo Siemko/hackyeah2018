11ca122cd6ec1c79f8937bbb39833bdfa365cb7a
Adrian Księżarczyk <adrian.ksiezarczyk@gorrion.pl>
2018-11-25 07:00:28 +0100

    filtering issues

---
+++ api/Orlen.Common/Const/IssueTypeName.cs
@@ -0,0 +1,10 @@
+﻿namespace Orlen.Common.Const
+{
+    public static class IssueTypeName
+    {
+        public static string MaxHeight = "MaxHeight";
+        public static string MaxWidth = "MaxWidth";
+        public static string MaxLength = "MaxLength";
+        public static string MaxWeight = "MaxWeight";
+    }
+}
