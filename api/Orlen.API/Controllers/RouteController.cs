8e6245b571564656308fd9f12938eb393de245c8
Dawid Świerczek <dawid.swierczek@gorrion.pl>
2018-11-25 10:40:54 +0100

    Fix ałuuuuuuuuuuuu

--- api/Orlen.API/Controllers/RouteController.cs
+++ api/Orlen.API/Controllers/RouteController.cs
@@ -33,7 +33,7 @@ namespace Orlen.API.Controllers
             return await routeService.GenerateRouteFromPoints(request);
         }
 
-        [HttpPost, Route("bus-route/{id}")]
+        [HttpGet, Route("bus-route/{id}")]
         public async Task<JContainer> GenerateRouteFromPoints(int id)
         {
             return await routeService.GetBusRoute(id);
